ead99146c476d35428610d620d3cf52d8689ee8f
harshal patil <harshalpatil1237@gmail.com>
2023-12-03 09:55:05 +0530

    .

---
+++ Sequence.js
@@ -0,0 +1,10 @@
+function Display() {
+    console.log("Monday");
+    console.log("Tuesday");
+    console.log("Wednesday");
+    console.log("Thursday");
+    console.log("Friay");
+    console.log("Saturday");
+    console.log("Sunday");
+}
+Display();
